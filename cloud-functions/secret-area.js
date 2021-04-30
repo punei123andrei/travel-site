d0bfde504a1ad21fea3814604214713e3b17c3dc
punei123andrei <punei.andrei123@gmail.com>
2021-04-30 15:27:36 +0300

    private settings

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,23 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "welcome to the secret area",
-  });
+  //run some logic
+  let body;
+  const secret = `
+  <h3>Welcome to the secret area</h3>
+  <p>The sky is blue</p>
+  `;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secret,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
