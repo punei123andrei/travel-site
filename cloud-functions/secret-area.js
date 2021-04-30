fceac765da8c485e0558f5dfa42fe883e1777eed
punei123andrei <punei.andrei123@gmail.com>
2021-04-30 15:08:08 +0300

    last change

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,4 @@
-exports.handler = function (event, contntext, callback) {
+exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
     body: "welcome to the secret area",
